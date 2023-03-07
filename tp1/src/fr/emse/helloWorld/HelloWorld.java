b2ffcd4b253422d265468febd34a29a1f512d5f6
Theo B <theo.besson@gmx.fr>
2023-03-07 22:17:52 +0100

    tp1a q7,8,9

--- tp1/src/fr/emse/helloWorld/HelloWorld.java
+++ tp1/src/fr/emse/helloWorld/HelloWorld.java
@@ -1,17 +1,38 @@
 package fr.emse.helloWorld;
 
+/**
+ * Cette classe est un test.
+ *
+ * @author Theo Besson
+ */
 public class HelloWorld {
 
+/**
+   * Le message à afficher
+   */
 private String message;
 
+/**
+   * Le constructeur de la classe HelloWorld
+   *
+   * @param message Le message a afficher
+   */
 public HelloWorld(String message) {
 	this.message=message;
 }
 
+   /**
+   * Cette methode renvoie une chaine de caracteres contenant le message
+   *
+   * @return Une chaine de caractere contenant le message
+   */
 public String getMessage() {
 	return this.message;
 }
 
+/**
+   * Cette methode affiche le message dans la console
+   */
 public static void main(String[] args){
    	HelloWorld h = new HelloWorld("Bonjour");
 	System.out.println(h.getMessage());
